3ca962cbda9abbaa65fd37f6d5bfdb3660281c78
dschreckling <dschrecking@users.noreply.github.com>
2017-05-10 09:07:04 +0200

    Allow for multiple inits

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var w = require("winston");
+w.level = process.env.LOG_LEVEL;
+
 var Policy = require("./Policy.js");
 var PolicySet = require("./PolicySet.js");
 var Entity = require("./Entity.js");
@@ -5,9 +8,18 @@ var Lock = require("./Lock.js");
 var Action = require("./Action.js");
 var Context = require("./Context");
 
+var Promise=require("bluebird");
+
+var initialized = false;
+
 var init = function(settings) {
+    if(initialized) {
+        w.info("ULocks have already been initialized. Ignore.");
+        return;
+    }    
+
     if(!settings)
-        throw new Error("Settings expected to initialized ULocks Framework");
+        throw new Error("Settings expected to initialize ULocks Framework");
     
     var toInit = [];
 
