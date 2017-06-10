d5b666752ef6ce0acf452cb0ef63eac55db01e9c
dschreckling <dschrecking@users.noreply.github.com>
2017-06-10 12:29:17 +0200

    Adding comments and improve code

--- Context.js
+++ Context.js
@@ -6,51 +6,51 @@ function valid(o) {
     return ((o !== undefined) && (o !== null));
 }
 
-/** 
+/**
  * Constructs or copies a context, required for the evaluation of policies, in particular of locks.
  * <b>NOTE:</b> All arguments below which are of type Object, are required to have the following format
  * <pre>
  *   { data: "The meta data describing the entity. This data
- *            will be used during lock evaluation.", 
- *     type: "The type of the entity specified in the 
- *            entity settings passed to the initialization 
+ *            will be used during lock evaluation.",
+ *     type: "The type of the entity specified in the
+ *            entity settings passed to the initialization
  *            of the ulocks module"
  *   }
  * </pre>
- * 
- * @param {Object|Context} context If only <code>context</code> is defined during the constructor call, the constructor 
+ *
+ * @param {Object|Context} context If only <code>context</code> is defined during the constructor call, the constructor
  * has the semantics of a copy constructor and copies the Context provided in this argument into a newly
- * constructed Context instance. In all other cases, it is the actual sender or subject of an access operation or of 
+ * constructed Context instance. In all other cases, it is the actual sender or subject of an access operation or of
  * a flow considered in the policy evaluation.
  * @param {Object} [receiver] Is the object of an access operation or the target where data should flow.
- * @param {Object} [message] The <code>message</code> or data flowing from sender to the object. Can only be specified if object is 
+ * @param {Object} [message] The <code>message</code> or data flowing from sender to the object. Can only be specified if object is
  * also specified correctly.
- * @param {isStatic} [isStatic] Specifies whether evaluation should take place during static analysis 
+ * @param {isStatic} [isStatic] Specifies whether evaluation should take place during static analysis
  * (<code>false</code>) or during dynamic analysis. Default is <code>false</code>
  * @class
  * @classdesc To evaluate them, locks in the Usage Locks framework may require attributes of any entity. Thus, a
  * lock must be put into a context when it is evaluated describing which view is taken in the evaluation. In general,
- * we assume that a <i>message</i> (or data) flows from a <i>sender</i> to a <i>receiver</i>. In this scenario, a lock 
- * may evaluate attributes of the sender, the receiver, or the message itself to compute control flow decisions. In this 
+ * we assume that a <i>message</i> (or data) flows from a <i>sender</i> to a <i>receiver</i>. In this scenario, a lock
+ * may evaluate attributes of the sender, the receiver, or the message itself to compute control flow decisions. In this
  * class we store the entity to be evaluated into <i>entity</i>. It is filled according to the view the context is in (changed
  * with the appropriate <code>set*Context</code> methods. In case, the lock requires more information, it can still use the
  * <code>sender</code>, <code>receiver</code>, <code>msg</code> members of this class.
  */
 function Context(context, receiver, message, isStatic) {
-    /** 
+    /**
      * Indicates whether this context is used for static analysis or in
      * a regular runtime environment.
      * @default false
      */
     this.isStatic = valid(isStatic) ? isStatic : false;
-    /** 
+    /**
      * Specifies the type of context. Changed by using the methods <code>set*Context</code>.
-     * @default Context.ContextTypes.normal
+     * @default Context.Types.normal
      */
-    this.type = ContextTypes.normal;
+    this.type = Context.Types.normal;
     /**
      * Object used during lock evaluation. Must have the general format <pre>
-     * { 
+     * {
      *   data: "The JSON description of the object",
      *   type: "The type of the entity specified in the configuration"
      * }
@@ -58,20 +58,20 @@ function Context(context, receiver, message, isStatic) {
      */
     this.entity = undefined;
     /**
-     * Contains the message object description of a message/data 
+     * Contains the message object description of a message/data
      * (similar to the entity opject) if the context is a message context
      */
     this.msg = undefined;
-    /** 
+    /**
      * Stores the current state of all locks in this context.
      */
     this.locks = undefined;
-    
+
     if(!valid(context))
         throw new Error("Invalid number or type of arguments passed to Context constructor!");
 
     // assume cloning of an existing object passed in first argument
-    if(valid(context) && !valid(receiver) && !valid(message) && !valid(isStatic)) {
+    if(!valid(receiver) && !valid(message) && !valid(isStatic)) {
         var c = context;
         this.sender = {
             data : c.sender.data,
@@ -81,19 +81,26 @@ function Context(context, receiver, message, isStatic) {
             data : c.receiver.data,
             type : c.receiver.type
         };
-	
+
         this.entity = context.entity ? ((context.entity == context.receiver) ? this.receiver : this.sender) : undefined;
-	
-        if(valid(c.msg)) 
+
+        if(valid(c.msg))
             this.msg = {
                 data : c.msg,
                 type : c.types
             };
-        
-        this.isStatic = c.isStatic;
-        this.locks = clone(c.locks);
-        this.setType(c.type);
-	
+
+        if(valid(c.isStatic))
+            this.isStatic = c.isStatic;
+
+        if(valid(c.locks))
+            this.locks = clone(c.locks);
+
+        if(valid(c.type))
+            this.setType(c.type);
+        else
+            this.setType(Context.Types.normal);
+
     } else if(valid(receiver)) {
         this.sender = {
             data : context.data,
@@ -104,55 +111,57 @@ function Context(context, receiver, message, isStatic) {
             data : receiver.data,
             type : receiver.type
         };
-        
+
         if(valid(message)) {
             this.msg = {
                 data : clone(message),
                 type : 'msg'
             };
         }
+
+        this.setType(Context.Types.normal);
     } else
-	throw new Error("Invalid number or type of arguments passed to Context constructor!");
+        throw new Error("Invalid number or type of arguments passed to Context constructor!");
 };
 
 // TODO: Can be changed from the outside. Must be protected!
-/** 
- * A context can have the following four types: <code>receiver</code>, <code>sender</code>, <code>message</code>, and <code>normal</code>. 
- * Through the <code>set*Context</code> these contexts are mapped to the classical subject and object entities. 
+/**
+ * A context can have the following four types: <code>receiver</code>, <code>sender</code>, <code>message</code>, and <code>normal</code>.
+ * Through the <code>set*Context</code> these contexts are mapped to the classical subject and object entities.
  * These mapping are enumerated below. They use the term <code>message</code> for data flowing.
- * 
+ *
  * <ul>
  *   <li>In <b>receiver</b> context the receiver is the subject and the message is the object. </li>
  *   <li>In <b>sender</b> context the sender is the subject and the message is the object. </li>
- *   <li>In <b>message</b> context the message is the subject and object itself. This context is for evaluating 
+ *   <li>In <b>message</b> context the message is the subject and object itself. This context is for evaluating
  *   locks specifically specified on the data itself</li>
- *   <li>In <b>normal</b> context the 
+ *   <li>In <b>normal</b> context the
  * </ul>
  * @static
  * @constant
  */
-const ContextTypes = {
+Context.Types = Object.freeze({
     receiver : "receiver",
     sender   : "sender",
     msg      : "message",
     normal   : "normal"
-};
+});
 
 // TODO: check whether this can be removed
 Context.prototype.setType = function(type) {
     switch(type) {
-    case ContextTypes.receiver:
-        setReceiverContext();
+    case Context.Types.receiver:
+        this.setReceiverContext();
         break;
-    case ContextTypes.sender:
-        setSenderContext();
+    case Context.Types.sender:
+        this.setSenderContext();
         break;
-    case ContextTypes.msg:
-        setMsgContext();
+    case Context.Types.msg:
+        this.setMsgContext();
         break;
-    case ContextTypes.normal:
-	setNormalContext();
-	break;
+    case Context.Types.normal:
+        this.setNormalContext();
+        break;
     default:
         throw new Error("Context: Unable to set context type. Unknown type '"+type+"'");
     }
@@ -161,40 +170,34 @@ Context.prototype.setType = function(type) {
 /** Puts the context into normal view  */
 Context.prototype.setNormalContext = function() {
     this.entity = this.sender;
-    this.type = Context.ContextTypes.normal;
+    this.type = Context.Types.normal;
 };
 
 /** Puts the context into receiver view  */
 Context.prototype.setReceiverContext = function() {
     this.entity = this.receiver;
-    this.type = Context.ContextTypes.receiver;
+    this.type = Context.Types.receiver;
 };
 
 /** Puts the context into sender view  */
 Context.prototype.setSenderContext = function() {
     this.entity = this.sender;
-    this.type = Context.ContextTypes.sender;
+    this.type = Context.Types.sender;
 };
 
 /** Puts the context into message view  */
 Context.prototype.setMsgContext = function() {
     this.entity = this.msg;
-    this.type = Context.ContextTypes.msg;
+    this.type = Context.Types.msg;
 };
 
 // TODO: review chaching functionality
 Context.prototype.getLockState = function(lock, subject) {
-    if(!lock)
+    if(!valid(lock) || !valid(lock.lock) || !valid(this.locks))
         return undefined;
 
-    if(!lock.path) 
-        return undefined;
-
-    if(lock.path == "closed")
+    if(lock.lock == "closed")
         return false;
-
-    if(!this.locks)
-        return undefined;
 
     var key = "global";
     if(subject) {
@@ -209,9 +212,9 @@ Context.prototype.getLockState = function(lock, subject) {
             key = subject.type + subject.data.id;
         }
     }
-    if(!this.locks[key]) 
+    if(!this.locks[key])
         return undefined;
-    
+
     var subjectContext = this.locks[key];
 
     if(subjectContext[lock.path] == undefined || subjectContext[lock.path] == null) {
@@ -221,18 +224,18 @@ Context.prototype.getLockState = function(lock, subject) {
             return subjectContext[lock.path];
         }
     }
-    
+
     var states = subjectContext[lock.path];
-    
+
     if(!lock.args || lock.args.length == 0) {
         if(states === true || states === false)
             return states;
     }
-    
+
     var strArg = "";
     for(var s in lock.args)
         strArg += lock.args[s] + ",";
-    
+
     if(states[strArg] == undefined || states[strArg] == null)
         return undefined;
     else
@@ -241,7 +244,7 @@ Context.prototype.getLockState = function(lock, subject) {
 
 // TODO: review chaching functionality
 Context.prototype.addLockState = function(lock, subject, value) {
-    if(!lock) 
+    if(!valid(lock))
         return;
 
     if(subject != undefined && (subject === false || subject === true) ) {
@@ -249,31 +252,31 @@ Context.prototype.addLockState = function(lock, subject, value) {
         subject = null;
     }
 
-    if(value == undefined || value == null)
+    if(!valid(value))
         value = true;
 
-    if(!this.locks) 
+    if(!valid(this.locks))
         this.locks = {};
 
     var key = "global";
     if(subject) {
-        if(!subject.type)
+        if(!valid(subject.type))
             throw new Error("Context: Invalid subject format!");
-        
+
         if(subject.type == "msg")
             key = "msg";
         else {
-            if(!subject.data)
+            if(!valid(subject.data))
                 return;
 
             key = subject.type + subject.data.id;
         }
     }
-    if(!this.locks[key])
+    if(!valid(this.locks[key]))
         this.locks[key] = {};
 
     var subjectContext = this.locks[key];
-    
+
     if(subjectContext[lock.path] == undefined || subjectContext[lock.path] == null) {
         subjectContext[lock.path] = {};
     } else {
@@ -283,7 +286,7 @@ Context.prototype.addLockState = function(lock, subject, value) {
     }
 
     // this must be a lock without arguments
-    if(!lock.args || lock.args.length == 0) {
+    if(!valid(lock.args) || lock.args.length == 0) {
         subjectContext[lock.path] = value;
         return;
     }
@@ -295,7 +298,7 @@ Context.prototype.addLockState = function(lock, subject, value) {
     var states = subjectContext[lock.path];
     if(states[strArg] == undefined || states[strArg] == null)
         states[strArg] = {};
-        
+
     states[strArg] = value;
 };
 
